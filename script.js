d76db3264ca4119e77f5be80613b4f4fcb934743
SlawekLysakowski <H4slo11!>
2022-04-14 21:39:36 +0200

    new front

--- script.js
+++ script.js
@@ -16,27 +16,29 @@ window.onload = function () {
    let correctNbr = 0;
    let wrongNbr = 0;
    let japToKor = true;
+   let wordsArray = [];
 
-   const wordsArray = () => [...new Array(10)]
-       .map(() => japaneseWords[Math.round(Math.random() * japaneseWords.length)]);
-   console.log(wordsArray());
-   const newWordsArray = function () {
-      if (japToKor) {
-         let  newRandomNumber = Math.trunc(Math.random() * japaneseWords.length);
-         return japaneseWords[newRandomNumber];
-      }
-      if (!japToKor) {
-         let  newRandomNumber = Math.trunc(Math.random() * japaneseWords.length);
-         return koreanWords[newRandomNumber];
-      }
+   const newWordsArray = function (vocab) {
+      if (japToKor) vocab = japaneseWords;
+      if (!japToKor) vocab = koreanWords;
+    wordsArray =  [...new Array(10)]
+          .map(() => vocab[Math.round(Math.random() * vocab.length)]);
+      console.log(wordsArray);
+      return wordsArray;
+   }
+   newWordsArray(japaneseWords);
+
+   for (let i = 0; i < wordsArray.length; i++) {
+      document.getElementById(`word-${i+1}`).textContent = wordsArray[i];
    }
 
-   document.getElementById('words').textContent = wordsArray();
+
+   document.getElementById('word-1').textContent = wordsArray[0];
    document.getElementById('word').textContent = randomWord;
    document.getElementById('polishWord').textContent = randomPolWord;
    document.getElementById('englishWord').textContent = randomEngWord;
    document.getElementById('correct').textContent = `Correct answers: ${correctNbr}`;
-   document.getElementById('wrong').textContent = `Wrong answers: ${wrongNbr}`;
+   document.getElementById('wrong').textContent = `Wrong answers:\u00A0 ${wrongNbr}`;
 
    // document.querySelector('.toggle').classList.remove('toggle:after ');
 
@@ -45,21 +47,21 @@ window.onload = function () {
    document.getElementById('translation').addEventListener('click', function () {
       if (japToKor) {
          japToKor = !japToKor;
-       document.getElementById('transDirection').textContent = 'Translation: KOR : JAP';
+       document.getElementById('translation').textContent = 'KOR : JAP';
        newWord();
       } else {
          japToKor = !japToKor;
-         document.getElementById('transDirection').textContent = 'Translation: JAP : KOR';
+         document.getElementById('translation').textContent = 'JAP : KOR';
          newWord();
       }
    })
 
    const hideShow = function (selectedSwitch, word) {
       document.getElementById(`${selectedSwitch}`).addEventListener("click", function () {
-      if (document.getElementById(`${word}`).style.display === "flex") {
+      if (document.getElementById(`${word}`).style.display === "grid") {
          document.getElementById(`${word}`).style.display = "none";
       } else  {
-         document.getElementById(`${word}`).style.display = "flex";
+         document.getElementById(`${word}`).style.display = "grid";
       }
       })
    }
@@ -110,7 +112,8 @@ window.onload = function () {
                correct();
             } else {
                document.getElementById('feedback').textContent =
-                   `wrong answer 😢 Correct answer was ${answerKor}`;
+                   `wrong answer! 😢 Correct answer was: \u00A0
+                   ` +  `  ${answerKor}`;
                wrongAnswer();
             }
 
@@ -120,7 +123,8 @@ window.onload = function () {
             } else {
 
                document.getElementById('feedback').textContent =
-                   `wrong answer 😢 Correct answer was ${answerJpy}`;
+                   `wrong answer! 😢 Correct answer was: \u00A0 
+                     ${answerJpy}`;
                wrongAnswer();
             }
          }
